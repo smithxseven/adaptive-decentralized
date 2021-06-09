28e449728e851a2c955bea92c353b759e58c5b1c
smith <804736@mnsd.org>
2021-06-09 11:56:00 -0400

    updated main version

--- cli/adaptive/__main__.py
+++ cli/adaptive/__main__.py
@@ -1,8 +1,7 @@
 import argparse
-from server import runtime
-runtimeDesc = 'Adaptive CLI --v.0.0.2--'
+runtimeDesc = 'Adaptive CLI --v.0.1.0--'
 messDesc = 'will eventually send message'
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=runtimeDesc)
-    parser.add_argument('-m', '--message', help=messDesc, required=runtime.username)
+    parser.add_argument('-m', '--message', help=messDesc) 
     args = parser.parse_args()
