8f59408160dc5e1f06e05ad58f53061d616df5a5
NATHAN SMITH (804736) <804736@mnsd.org>
2021-06-09 10:54:30 -0400

    updated version number

--- cli/setup.py
+++ cli/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'adaptive',
-    version = '0.0.2',
+    version = '0.1.0',
     packages = ['adaptive'],
     entry_points  = {
         'console_scipts': [
